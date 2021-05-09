1869861345be03703f8b5da7aae98f6b70476d07
root <root@Winuino.localdomain>
2021-05-09 14:04:38 +0200

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,57 @@
+// import styled from "styled-components";
+import { Link } from "react-router-dom";
+// import { Link as LinkR } from "react-router-dom";
+// import { Link as LinkS } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import "./Styles.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav">
+        <div className="nav-bar-container">
+          <Link className="nav-logo" to="/">
+            doola
+          </Link>
+          <div className="mobile-icon">
+            <FaBars />
+          </div>
+          <ul className="nav-menu">
+            <li className="nav-link">
+              <Link className="nav-links" to="about">
+                About
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link className="nav-links" to="about">
+                Skills
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link className="nav-links" to="about">
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link className="nav-links" to="about">
+                CV
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link className="nav-links" to="about">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <div className="nav-btn">
+            <Link className="nav-btn-link" to="/signin/">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
